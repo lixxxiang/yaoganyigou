a1c3b4c3180ec048f885df444448ed16a1cd4cd1
lixxiang <1548806494@qq.com>
2019-01-24 16:45:32 +0800

    --

--- app/src/main/java/com/android/lixiang/yaoganyigou/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/android/lixiang/yaoganyigou/ui/fragment/HomeFragment.kt
@@ -10,18 +10,26 @@ import com.android.lixiang.base.ui.fragment.BaseMvpFragment
 import com.android.lixiang.yaoganyigou.R
 import com.android.lixiang.yaoganyigou.presenter.HomePresenter
 import com.android.lixiang.yaoganyigou.presenter.data.bean.HomeItem
+import com.android.lixiang.yaoganyigou.presenter.data.bean.HomeMultiItem
 import com.android.lixiang.yaoganyigou.presenter.injection.component.DaggerHomeFragmentComponent
 import com.android.lixiang.yaoganyigou.presenter.injection.module.HomeModule
 import com.android.lixiang.yaoganyigou.presenter.view.HomeView
 import com.android.lixiang.yaoganyigou.ui.adapter.HomeAdapter
+import com.android.lixiang.yaoganyigou.ui.adapter.HomeMultiAdapter
+import com.orhanobut.logger.Logger
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : BaseMvpFragment<HomePresenter>(), HomeView {
 
     private var homeAdapter: HomeAdapter? = null
+    private var homeMultiAdapter: HomeMultiAdapter? = null
+
     private var mLayoutManager: LinearLayoutManager? = null
     private var homeItem: HomeItem? = null
+    private var homeMultiItem: HomeMultiItem? = null
     private var mList: MutableList<HomeItem>? = mutableListOf()
+    private var mMultiList: MutableList<HomeMultiItem>? = mutableListOf()
+
     override fun injectComponent() {
         DaggerHomeFragmentComponent.builder().fragmentComponent(fragmentComponent).homeModule(HomeModule()).build()
             .inject(this)
@@ -41,15 +49,24 @@ class HomeFragment : BaseMvpFragment<HomePresenter>(), HomeView {
         test()
         mLayoutManager = LinearLayoutManager(context)
         homeAdapter = HomeAdapter(R.layout.item_test, mList)
+        homeMultiAdapter = HomeMultiAdapter(mList)
+        Logger.d(homeMultiItem)
         mHomeRV.layoutManager = mLayoutManager
-        mHomeRV.adapter = homeAdapter
+        mHomeRV.adapter = homeMultiAdapter
 
     }
 
     private fun test() {
-        homeItem = HomeItem()
-        homeItem?.s = "hello"
-        for (i in 0 until 10)
-            mList?.add(homeItem!!)
+//        homeItem = HomeItem()
+//        for (i in 0 until 10) {
+//            homeItem?.s = "hello$i"
+//            mList?.add(homeItem!!)
+//        }
+
+        homeMultiItem = HomeMultiItem()
+        for (i in 0 until 10) {
+            homeMultiItem?.s = "hello$i"
+            mMultiList?.add(homeMultiItem!!)
+        }
     }
 }
